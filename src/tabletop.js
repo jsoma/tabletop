584d5dd8d4cb7eb6b472dc85a319cc2768f00d1f
Scott Seaward <sseaward@nypublicradio.org>
2012-02-06 16:42:13 -0500

    Multiple tabletops supported.  Added simple example for multiple tabletops.

--- src/tabletop.js
+++ src/tabletop.js
@@ -8,8 +8,10 @@
     Initialize with Tabletop.init('0AjAPaAU9MeLFdHUxTlJiVVRYNGRJQnRmSnQwTlpoUXc')
   */
 
-  var Tabletop = global.Tabletop = global.Tabletop || function(options) {
-    if(this === global) {
+  "use strict";
+
+  var Tabletop = global.Tabletop = function(options) {
+    if(!this || this === global) {
       return new Tabletop(options);
     }
 
@@ -28,6 +30,7 @@
     if(/key=/.test(this.key)) {
       if(this.debug)
         console.debug("You passed a key as a URL! Attempting to parse.");
+
       this.key = this.key.match("key=(.*?)&")[1];
     }
 
@@ -42,11 +45,12 @@
     this.models = {};
     this.model_names = [];
 
-    var callback = "Tabletop.loadSheets";
-    var json_url = "https://spreadsheets.google.com/feeds/worksheets/" + this.key + "/public/basic?alt=json-in-script&callback=" + callback;
+    var json_url = "https://spreadsheets.google.com/feeds/worksheets/" + this.key + "/public/basic?alt=json-in-script";
 
-    this.injectScript(json_url);
+    this.injectScript(json_url, this.loadSheets);
   };
+
+  Tabletop.callbacks = {};
 
   Tabletop.init = function(options) {
     return new Tabletop(options);
@@ -54,30 +58,40 @@
 
   Tabletop.prototype = {
 
-    /* 
+    /*
       Insert the URL into the page as a script tag. Once it's loaded the spreadsheet data
       it triggers the callback. This helps you avoid cross-domain errors
       http://code.google.com/apis/gdata/samples/spreadsheet_sample.html
 
       Let's be plain-Jane and not use jQuery or anything.
-    */  
-    injectScript: function(url) {
-      var script = document.createElement('script');
-      script['type'] = "text/javascript";
-      script['src'] = url;
-      document.getElementsByTagName('body')[0].appendChild(script);
+    */
+    injectScript: function(url, callback) {
+      var script = document.createElement('script'),
+          self = this,
+          callbackName = 'tt' + (+new Date()) + (Math.floor(Math.random()*100000));
+      Tabletop.callbacks[ callbackName ] = function () {
+        var args = Array.prototype.slice.call( arguments, 0 );
+        callback.apply(self, args);
+        script.parentNode.removeChild(script);
+        delete Tabletop.callbacks[callbackName];
+      };
+      url = url + "&callback=" + 'Tabletop.callbacks.' + callbackName;
+      script.src = url;
+      document.getElementsByTagName('script')[0].parentNode.appendChild(script);
     },
 
     /*
       What gets send to the callback
-        if simpleSheet == true, then don't return an array of Tabletop.this.models, 
-          only return the first one's elements
+      if simpleSheet == true, then don't return an array of Tabletop.this.models,
+      only return the first one's elements
     */
-    tabletop.data: function() {
+    data: function() {
+      if(this.model_names.length === 0) {
+        return undefined;
+      }
       if(this.simpleSheet) {
-        if(this.models.length > 1 && this.debug)
+        if(this.model_names.length > 1 && this.debug)
           console.debug("WARNING You have more than one sheet but are using simple sheet mode! Don't blame me when something goes wrong.");
-
         return this.models[ this.model_names[0] ].all();
       } else {
         return this.models;
@@ -93,12 +107,12 @@
       Used as a callback for the worksheet-based JSON
     */
     loadSheets: function(data) {
+      var i;
       this.sheetsToLoad = data.feed.entry.length;
       for(i = 0; i < data.feed.entry.length; i++) {
         var sheet_id = data.feed.entry[i].link[3].href.substr(-3, 3);
-        var callback = "Tabletop.loadSheet";
-        var json_url = "https://spreadsheets.google.com/feeds/list/" + this.key + "/" + sheet_id + "/public/values?alt=json-in-script&callback=" + callback;
-        this.injectScript(json_url);
+        var json_url = "https://spreadsheets.google.com/feeds/list/" + this.key + "/" + sheet_id + "/public/values?alt=json-in-script";
+        this.injectScript(json_url, this.loadSheet);
       }
     },
 
@@ -108,8 +122,8 @@
       .sheets('Sheet1') gets you the sheet named Sheet1
     */
     sheets: function(sheetName) {
-      if(sheetName == undefined)
-        return this.models
+      if(typeof sheetName === "undefined")
+        return this.models;
       else
         return this.models[ sheetName ];
     },
@@ -120,13 +134,13 @@
       Used as a callback for the list-based JSON
     */
     loadSheet: function(data) {
-      var model = new this.Model( { data: data, 
+      var model = new Tabletop.Model( { data: data, 
                                     parseNumbers: this.parseNumbers,
                                     postProcess: this.postProcess } );
       this.models[ model.name ] = model;
       this.model_names.push(model.name);
       this.sheetsToLoad--;
-      if(this.sheetsToLoad == 0)
+      if(this.sheetsToLoad === 0)
         this.doCallback();
     },
 
@@ -136,7 +150,7 @@
       Tests this.sheetsToLoad just in case a race condition happens to show up
     */
     doCallback: function() {
-      if(this.sheetsToLoad == 0)
+      if(this.sheetsToLoad === 0)
         this.callback(this.data());
     }
   };
@@ -148,14 +162,14 @@
     Options should be in the format { data: XXX }, with XXX being the list-based worksheet
   */
   Tabletop.Model = function(options) {
+    var i, j;
     this.column_names = [];
     this.name = options.data.feed.title.$t;
     this.elements = [];
 
     for(var key in options.data.feed.entry[0]){
-      if(/^gsx/.test(key)) {
+      if(/^gsx/.test(key))
         this.column_names.push( key.replace("gsx$","") );
-      }
     }
 
     for(i = 0; i < options.data.feed.entry.length; i++) {
@@ -164,13 +178,12 @@
 
       for(j = 0; j < this.column_names.length; j++) {
         var cell = source[ "gsx$" + this.column_names[j] ];
-        if(options.parseNumbers && cell.$t != '' && !isNaN(cell.$t)) {
+        if(options.parseNumbers && cell.$t !== '' && !isNaN(cell.$t))
           element[ this.column_names[j] ] = +cell.$t;
-        } else {
+        else
           element[ this.column_names[j] ] = cell.$t;
-        };
       }
-      if(element.rowNumber == undefined)
+      if(element.rowNumber === undefined)
         element.rowNumber = i + 1;
       if( options.postProcess )
         options.postProcess(element);
@@ -191,7 +204,8 @@
       Return the elements as an array of arrays, instead of an array of objects
     */
     toArray: function() {
-      var array = [];
+      var array = [],
+          i, j;
       for(i = 0; i < this.elements.length; i++) {
         var row = [];
         for(j = 0; j < this.column_names.length; j++) {
@@ -203,4 +217,4 @@
     }
   };
 
-}(this);
+})(this);
