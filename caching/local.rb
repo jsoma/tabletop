ffb19c047d1c3a254f6a09e5d44b7bb9f15e702c
Evan Hensleigh <futuraprime@gmail.com>
2013-05-30 15:37:59 -0400

    Switched the included caching script to also use pipe delimiting

--- caching/local.rb
+++ caching/local.rb
@@ -20,7 +20,7 @@ sheet_ids = base_json_content.scan(/\/public\/basic\/(\w*)/).flatten.uniq
 sheet_ids.each do |sheet_id|
   sheet_url = "https://spreadsheets.google.com//feeds/list/#{key}/#{sheet_id}/public/values?alt=json-in-script&sq=&callback=Tabletop.singleton.loadSheet"
   content = open(sheet_url).read
-  File.open("#{key}-#{sheet_id}", 'w') { |f| f.write(content) } 
+  File.open("#{key}|#{sheet_id}", 'w') { |f| f.write(content) } 
 end
 
 File.open("#{key}", 'w') { |f| f.write(base_json_content) } 
