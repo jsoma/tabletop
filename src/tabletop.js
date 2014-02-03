b9f02dd9e69dc868f620f244ca961a30e8333299
Alan Palazzolo <apalazzolo@minnpost.com>
2014-02-03 12:54:42 -0600

    Making nodeJS check even more specific.

--- src/tabletop.js
+++ src/tabletop.js
@@ -2,7 +2,7 @@
   "use strict";
 
   var inNodeJS = false;
-  if (typeof module !== 'undefined' && module.exports) {
+  if (typeof window === 'undefined' && typeof module !== 'undefined' && module.exports) {
     inNodeJS = true;
     var request = require('request');
   }
