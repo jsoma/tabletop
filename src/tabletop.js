c3fb4c46fd2640c2c480149ab07a147a86dc1ee2
Jonathan Soma <jonathan.soma@gmail.com>
2014-04-27 11:40:08 -0400

    No longer throw an error when new-style spreadsheets are passed in

--- src/tabletop.js
+++ src/tabletop.js
@@ -96,9 +96,8 @@
     }
 
     if(/pubhtml/.test(this.key)) {
-      alert("You passed a new Google Spreadsheets url as the key! This won't work yet, you'll need to change back to old Sheets.");
+      this.log("You passed a new Google Spreadsheets url as the key! Attempting to parse.");
       this.key = this.key.match("d\\/(.*?)\\/pubhtml")[1];
-      console.log(this.key);
     }
 
     if(!this.key) {
