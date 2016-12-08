9c4b4443129d5ba61cb94dc8c06f946d57cc87e1
Jonathan Soma <jonathan.soma@gmail.com>
2016-12-08 10:34:05 -0500

    Some dead simple tests so I can start accepting pull requests willy-nilly

---
+++ test/sheets.js
@@ -0,0 +1,81 @@
+var Tabletop = require('../src/tabletop')
+var assert = require('assert');
+
+var sheets = {
+  '2011': 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE&output=html',
+  '2014': 'https://docs.google.com/spreadsheets/d/1Vmj7tj64bz1cFRnbCJCAAXufonxIVOKqhZDTfPOvFTU/pubhtml',
+  '2016': 'https://docs.google.com/spreadsheets/d/1sbyMINQHPsJctjAtMW0lCfLrcpMqoGMOJj6AN-sNQrc/pubhtml'
+}
+
+describe('2011-ish era spreadsheets', function() {
+  it('pulls in a single spreadsheet', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2011'],
+      callback: function(data, tabletop) {
+        assert(data.length, 3, '3 rows were pulled in')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+
+  it('pulls in a multiple spreadsheets', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2011'],
+      callback: function(data, tabletop) {
+        assert(tabletop.sheets('Sheet1'), 'can access Sheet1')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+});
+
+describe('2014-ish era spreadsheets', function() {
+  it('pulls in a single spreadsheet', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2014'],
+      callback: function(data, tabletop) {
+        assert(data.length, 2, '2 rows were pulled in')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+
+  it('pulls in a multiple spreadsheets', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2014'],
+      callback: function(data, tabletop) {
+        assert(tabletop.sheets('Sheet1'), 'can access Sheet1')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+});
+
+
+describe('2016-ish era spreadsheets', function() {
+  it('pulls in a single spreadsheet', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2016'],
+      callback: function(data, tabletop) {
+        assert(data.length, 4, '4 rows were pulled in')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+
+  it('pulls in a multiple spreadsheets', function(done) {
+    var tabletop = Tabletop.init({
+      key: sheets['2016'],
+      callback: function(data, tabletop) {
+        assert(tabletop.sheets('books'), 'can access books')
+        done()
+      },
+      simpleSheet: true
+    })
+  })
+});
