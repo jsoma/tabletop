3f02a81e1e10662205cf70958c6aa91312633b9b
Jonathan Soma <jonathan.soma@gmail.com>
2018-02-21 16:23:16 -0500

    Bump to 1.5.4

--- test/sheets.js
+++ test/sheets.js
@@ -5,10 +5,34 @@ var assert = require('assert');
 var sheets = {
   '2011': 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE&output=html',
   '2014': 'https://docs.google.com/spreadsheets/d/1Vmj7tj64bz1cFRnbCJCAAXufonxIVOKqhZDTfPOvFTU/pubhtml',
-  '2016': 'https://docs.google.com/spreadsheets/d/1sbyMINQHPsJctjAtMW0lCfLrcpMqoGMOJj6AN-sNQrc/pubhtml'
+  '2016': 'https://docs.google.com/spreadsheets/d/1sbyMINQHPsJctjAtMW0lCfLrcpMqoGMOJj6AN-sNQrc/pubhtml',
+  '2018': 'https://docs.google.com/spreadsheets/d/1Io6W5XitNvifEXER9ECTsbHhAjXsQLq6VEz7kSPDPiQ/edit?usp=sharing'
 }
 
 function testTabletop(Tabletop) {
+  describe('2018-02 spreadsheets', function() {
+    it('pulls in a single spreadsheet', function(done) {
+      var tabletop = Tabletop.init({
+        key: sheets['2018'],
+        callback: function(data, tabletop) {
+          assert(data.length, 3, '3 rows were pulled in')
+          done()
+        },
+        simpleSheet: true
+      })
+    })
+
+    it('pulls in a multiple spreadsheets', function(done) {
+      var tabletop = Tabletop.init({
+        key: sheets['2018'],
+        callback: function(data, tabletop) {
+          assert(tabletop.sheets('Sheet1'), 'can access Sheet1')
+          done()
+        }
+      })
+    })
+  });
+
   describe('2011-ish era spreadsheets', function() {
     it('pulls in a single spreadsheet', function(done) {
       var tabletop = Tabletop.init({
